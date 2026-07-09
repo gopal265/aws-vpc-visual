efe6c3cd53418dccbfb2a22f522adca8b011f022
Gopal Reddy <84660174+gopal265@users.noreply.github.com>
2026-07-09 11:30:44 +0000

    test cases fix

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,9 +3,9 @@ import { describe, it, expect } from 'vitest'
 import App from './App'
 
 describe('App Component', () => {
-  it('renders the main heading', () => {
+  it('renders the App component', () => {
      const { container } = render(<App />)
-    const element = container.querySelector('.framewor')
+    const element = container.querySelector('.framework')
     // Asserts that an element is in the document
     expect(element).toBeInTheDocument()
   })
