0e7c274114a70d446bc1683f92c04ac773f2218b
Gopal Reddy <84660174+gopal265@users.noreply.github.com>
2026-07-09 11:25:18 +0000

    Linting and Test cases setup

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    // simulates a broswer enviroment in node js
+    enviroment: 'jsdom',
+    // Allows you to use describe, it, expect globally without importing them everytime
+    globals: true, 
+    setupFiles: './src/tests/setup.js'
+  }
 })
