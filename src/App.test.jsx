d344ffd44ea493b29764b4d274b288f2308ab97e
Gopal Reddy <84660174+gopal265@users.noreply.github.com>
2026-07-09 11:29:30 +0000

    integrate test cases to workflow

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,7 +5,7 @@ import App from './App'
 describe('App Component', () => {
   it('renders the main heading', () => {
      const { container } = render(<App />)
-    const element = container.querySelector('.framework')
+    const element = container.querySelector('.framewor')
     // Asserts that an element is in the document
     expect(element).toBeInTheDocument()
   })
