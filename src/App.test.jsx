f05ae3aebbf1fa2e6dead61098b683a943da1008
gopal265 <gopal265.reddy@gmail.com>
2026-08-10 11:43:19 +0000

    added caching npm

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,7 +5,7 @@ import App from './App'
 describe('App Component', () => {
   it('renders the App component', () => {
      const { container } = render(<App />)
-    const element = container.querySelector('.framewor')
+    const element = container.querySelector('.framework')
     // Asserts that an element is in the document
     expect(element).toBeInTheDocument()
   })
